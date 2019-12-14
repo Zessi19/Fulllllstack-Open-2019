46dba7192de88fdcf422b49509ae3c591ef486f6
Joonas <joonas.jussila@helsinki.fi>
2019-12-14 13:50:50 +0200

    osa8 part A

---
+++ osa8/README_osa8.md
@@ -0,0 +1,7 @@
+# Osa 8
+
+## Part A (7/7)
+
+GraphQL backend seuraa materiaalin mallia ja sovellus toimii esimerkki tulostusten olettamalla tavalla (myös erikoistapauksissa).
+
+## Part B (/5)
---
+++ osa8/kirjatGraphQl/temp.js
@@ -0,0 +1,5 @@
+/*
+ * It would be more sensible to assosiate book and the author by saving 
+ * the author id instead of the name to the book.
+ * For simplicity we however save the author name.
+*/
